df1fd45d4e6dbed73e4955486daaf5b336a3727f
Emory.Du <Emory163@163.com>
2023-08-16 17:14:50 +0800

    feat(core): add gin error response

---
+++ pkg/core/core.go
@@ -0,0 +1,42 @@
+package core
+
+import (
+	"github.com/emorydu/errors"
+	"github.com/emorydu/log"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// ErrResponse defines the return message when an error occurred.
+// Reference will be omitted if it does not exist.
+// swagger:model
+type ErrResponse struct {
+	// Code defines the business error code.
+	Code int `json:"code"`
+
+	// Message contains be detail of this message.
+	// This message is suitable to be exposed to external.
+	Message string `json:"message"`
+
+	// Reference returns the reference document which maybe useful to solve this error.
+	Reference string `json:"reference,omitempty"`
+}
+
+// WriteResponse write an error or the response data into http response body.
+// It use errors.ParseCoder to parse any error into errors.Coder
+// errors.Coder contains error code, user-safe error message and http status code.
+func WriteResponse(c *gin.Context, err error, data interface{}) {
+	if err != nil {
+		log.Errorf("%#+v", err)
+		coder := errors.ParseCoder(err)
+		c.JSON(coder.HTTPStatus(), ErrResponse{
+			Code:      coder.Code(),
+			Message:   coder.String(),
+			Reference: coder.Reference(),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, data)
+}
---
+++ pkg/core/doc.go
@@ -0,0 +1,2 @@
+// Package core implements some core functions used by apimachinery.
+package core // import "github.com/emorydu/pkg/core"
